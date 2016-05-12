5dc69e127ddbb6557e951cb6f42e900fd767d0a6
David Sterry <dsterry@uber.com>
2016-05-11 18:10:56 -0700

    config change

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Owner(db.Model):
