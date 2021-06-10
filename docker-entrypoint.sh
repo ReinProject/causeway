69bc04e8b4a281668123e08a4d6bf657c0bc384a
Arsen A. Gutsal <aahutsal@gmail.com>
2021-06-10 20:19:27 +0300

    Update (#20)
    Better a late merge than never 😄

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,10 @@
 for f in docker-entrypoint.d/*; do
     case "$f" in
         *.sh)     echo "$0: running $f"; . "$f" ;;
-        *.sql)    echo "$0: running $f"; psql -h localhost causeway < "$f"; echo ;;
+        *.sql)    echo "$0: running $f"; psql -h db causeway < "$f"; echo ;;
         *.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${psql[@]}"; echo ;;
         *)        echo "$0: ignoring $f" ;;
     esac
+    sleep 5;
     echo
 done
